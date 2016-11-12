1e04c5b77eeda4d3901fdcaf1bb18e015a4225e3
teenyrobots <hellojoycerice@gmail.com>
2016-11-12 09:08:16 -0500

    prototype
    Initial prototype of Lai Generator

---
+++ data.js
@@ -0,0 +1,168 @@
+// Panel objects for Lais generation
+var laiPanels = [
+	/*{
+		brutal: 1,
+		passion: 0,
+		src: "panels/brutal1.png"
+	},
+
+	{
+		brutal: 2,
+		passion: 0,
+		src: "panels/brutal2.png"
+	},
+
+	{
+		brutal: 3,
+		passion: 0,
+		src: "panels/brutal3.png"
+	},
+
+	{
+		brutal: 4,
+		passion: 0,
+		src: "panels/brutal4.png"
+	},
+
+	{
+		brutal: 5,
+		passion: 0,
+		src: "panels/brutal5.png"
+	},*/
+
+	{
+		brutal: 0,
+		passion: 0,
+		src: "panels/nuns1.png"
+	},
+
+	{
+		brutal: 0,
+		passion: 0,
+		src: "panels/nuns2.png"
+	},
+
+	{
+		brutal: 0,
+		passion: 1,
+		src: "panels/towers.png"
+	},
+
+	{
+		brutal: 2,
+		passion: 0,
+		src: "panels/werewolf.png"
+	},
+
+	{
+		brutal: 0,
+		passion: 3,
+		src: "panels/neverBetray.png"
+	},
+
+	{
+		brutal: 4,
+		passion: 0,
+		src: "panels/anOmen.png"
+	},
+
+	{
+		brutal: 5,
+		passion: 0,
+		src: "panels/realDad.png"
+	},
+
+	{
+		brutal: 1,
+		passion: 0,
+		src: "panels/hideClothes.png"
+	},
+
+	{
+		brutal: 5,
+		passion: 0,
+		src: "panels/nightingale.png"
+	},
+
+	{
+		brutal: 3,
+		passion: 0,
+		src: "panels/boat.png"
+	},
+
+	{
+		brutal: 1,
+		passion: 0,
+		src: "panels/tower.png"
+	},
+
+	{
+		brutal: 1,
+		passion: 5,
+		src: "panels/dieFuckMe.png"
+	},
+
+	{
+		brutal: 0,
+		passion: 4,
+		src: "panels/allWomens.png"
+	},
+
+	{
+		brutal: 0,
+		passion: 1,
+		src: "panels/broGrabs.png"
+	},
+
+	{
+		brutal: 2,
+		passion: 0,
+		src: "panels/inspireLoyalty.png"
+	},
+
+	{
+		brutal: 0,
+		passion: 3,
+		src: "panels/powerDynamic.png"
+	},
+
+	{
+		brutal: 2,
+		passion: 4,
+		src: "panels/stewardsWife.png"
+	},
+
+	{
+		brutal: 3,
+		passion: 3,
+		src: "panels/killHusband.png"
+	},
+
+	{
+		brutal: 4,
+		passion: 0,
+		src: "panels/brobaths.png"
+	}
+]
+
+var unbrutalPanels = [];
+	for (i = 0; i < laiPanels.length; i++) {
+		if (laiPanels[i].brutal == 0) {
+			unbrutalPanels.push(laiPanels[i]);
+		}
+	}
+
+var moreBrutalPanels = [];
+	for (i = 0; i < laiPanels.length; i++) {
+		if (laiPanels[i].brutal > 2) {
+			moreBrutalPanels.push(laiPanels[i]);
+		}
+	}
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,74 @@
+// Javascript for Lais of Marie de France
+
+$(document).ready(function(){
+	$("#aboutCopy").hide();
+	$("#aboutHead").click(function(){
+		$("#aboutCopy").show();
+	});
+})
+
+
+function generateLai() {
+	//get player's inputed brutality, which can be between 8 and 40
+	var playerBrutality = document.getElementById("playerBrutality").value;
+	console.log(playerBrutality);
+
+	//how brutal the story is so far
+	var overallBrutality = 0;
+
+	// case statement for different levels of brutality
+	// case 0: 0-15
+		// choose random panels but don't exceed player brutality; switch to nuns
+	// case 1: 16-29
+		// choose more brutal panels but don't exceed 1/2 player brutality; switch to all random panels; switch to nuns
+	// case 3: 30-40
+		// choose more brutal panels but don't exceed 
+
+
+		/* switch (true) {
+			case (playerBrutality < 15 ): 
+		} */
+
+	var partPlayerBrutality = (playerBrutality * .8);
+
+	console.log(partPlayerBrutality);
+
+	for (var i = 0; i < 8; i++) {
+		if (overallBrutality < partPlayerBrutality) {
+			var randomPanel = Math.floor(Math.random() * moreBrutalPanels.length) + 0;
+			var panel = document.createElement("img");
+			panel.src = moreBrutalPanels[randomPanel].src;
+			document.getElementById("lai").appendChild(panel);
+			overallBrutality += moreBrutalPanels[randomPanel].brutal;
+			moreBrutalPanels.splice(randomPanel, 1 );
+		} else if (overallBrutality < playerBrutality) {
+			//adds new random panel:
+			var randomPanel = Math.floor(Math.random() * laiPanels.length) + 0;
+			var panel = document.createElement("img");
+			panel.src = laiPanels[randomPanel].src;
+			document.getElementById("lai").appendChild(panel);
+			//tracks brutality of lai thus far:
+			overallBrutality += laiPanels[randomPanel].brutal;
+			laiPanels.splice(randomPanel, 1 );
+		} else {
+			var randomPanel = Math.floor(Math.random() * unbrutalPanels.length) + 0;
+			var panel = document.createElement("img");
+			panel.src = unbrutalPanels[randomPanel].src;
+			document.getElementById("lai").appendChild(panel);
+			unbrutalPanels.splice(randomPanel, 1 );
+		}
+	}
+
+	console.log(overallBrutality);
+
+	var btn = document.getElementById("btn");
+	btn.disabled = true;
+
+}
+
+
+
+// NOTES
+
+// Returns a random integer between min and max:
+// Math.floor(Math.random() * (max - min + 1)) + min;
