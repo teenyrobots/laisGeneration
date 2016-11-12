f2247d1ec44d53e2110f97b9892372e8abba99ea
teenyrobots <hellojoycerice@gmail.com>
2016-11-12 15:52:51 -0500

    generateLai2-start
    Started the new generate Lai function that culls from one list instead
    of making a bunch of separate lists

--- script.js
+++ script.js
@@ -9,6 +9,42 @@ $(document).ready(function(){
 })
 
 
+function generateLai2() {
+	var playerBrutality = document.getElementById("playerBrutality").value;
+	//var playerPassion = document.getElementById("playerPassion").value;
+	//var playerKnights = document.getElementById("playerKnights").value;
+
+	var overallBrutality = 0;
+	var overallPassion = 0;
+//	var overallKnights = i haven't figured this out yet;
+
+//	start with a chunk that immediately splices everything that is going to be too brutal?
+
+	for (var i=0; i<8; i++) {
+		var randomPanel = Math.floor(Math.random() * laiPanels.length) + 0;
+		var panel = document.createElement("img");
+		panel.src = laiPanels[randomPanel].src;
+		document.getElementById("lai").appendChild(panel);
+		laiPanels.splice(randomPanel, 1);
+
+		overallBrutality += laiPanels[randomPanel].brutal;
+		var remainingBrutality = playerBrutality - overallBrutality;
+
+//NOTES
+//		if diff between playerBrutality and overallBrutality is less than x, remove all panels from the list that are too brutal
+//		ok this is going to involve some actual maths here.
+//		the things that remain in the list should have brutality such that they can meet the target
+//		if the player brutality is low, the very brutal things should be removed
+// 		if the player brutality is high, the not-very-brutal things should be removed
+//		the range is 0 to 40. the range per panel is 0 to 5. 8 * 5 = 40.
+//		
+
+//		if diff between playerPassion and overallPassion is less than x , remove all panels from the list that are too passionate
+//		if there are too many knights, don't introduce any new nights (string based?)
+	}
+}
+
+
 function generateLai() {
 	//get player's inputed brutality, which can be between 8 and 40
 	var playerBrutality = document.getElementById("playerBrutality").value;
@@ -73,3 +109,10 @@ function generateLai() {
 
 // Returns a random integer between min and max:
 // Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+
+
+
+
+
