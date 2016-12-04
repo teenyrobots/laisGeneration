a2886bf0bacf450b3937f8b13fbad99a916f6895
teenyrobots <hellojoycerice@gmail.com>
2016-12-03 21:39:58 -0500

    brokenLastPanel
    last panel property not accessible

--- data.js
+++ data.js
@@ -1,171 +1,209 @@
-// Panel objects for Lais generation
 var laiPanels = [
-	{
-		brutal: 0,
-		passion: 0,
-		src: "panels/nuns1.png",
-		alt: "one nun saying 'I guess i'll become a nun'",
-		beginning: false,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 0,
-		passion: 0,
-		src: "panels/nuns2.png",
-		alt: "two happy nuns",
-		beginning: false,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 0,
-		passion: 2,
-		src: "panels/towers.png",
-		alt: "two towers facing each other, the lady says 'i'm so in love' and the dude says 'yah me too'",
-		beginning: true,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 4,
-		passion: 0,
-		src: "panels/werewolf.png",
-		beginning: false,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 0,
-		passion: 6,
-		src: "panels/neverBetray.png",
-		beginning: true,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 8,
-		passion: 0,
-		src: "panels/anOmen.png",
-		beginning: true,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 10,
-		passion: 0,
-		src: "panels/realDad.png",
-		beginning: false,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 2,
-		passion: 0,
-		src: "panels/hideClothes.png",
-		beginning: true,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 10,
-		passion: 0,
-		src: "panels/nightingale.png",
-		beginning: false,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 6,
-		passion: 0,
-		src: "panels/boat.png",
-		beginning: false,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 2,
-		passion: 0,
-		src: "panels/tower.png",
-		beginning: true,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 2,
-		passion: 10,
-		src: "panels/dieFuckMe.png",
-		beginning: false,
-		middle: true,
-		end: false
-	},
-
-	{
-		brutal: 0,
-		passion: 8,
-		src: "panels/allWomens.png",
-		beginning: false,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 0,
-		passion: 2,
-		src: "panels/broGrabs.png",
-		beginning: false,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 4,
-		passion: 0,
-		src: "panels/inspireLoyalty.png",
-		beginning: true,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 0,
-		passion: 6,
-		src: "panels/powerDynamic.png",
-		beginning: false,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 6,
-		passion: 6,
-		src: "panels/killHusband.png",
-		beginning: false,
-		middle: true,
-		end: true
-	},
-
-	{
-		brutal: 8,
-		passion: 0,
-		src: "panels/brobaths.png",
-		beginning: false,
-		middle: true,
-		end: true
-	}
+ {
+   "ID": 0,
+   "src": "panels/anOmenDeer.png",
+   "brutal": 8,
+   "passion": 2,
+   "beginning": true,
+   "middle": true,
+   "end": false
+ },
+ {
+   "ID": 1,
+   "src": "panels/nunsSayNo.png",
+   "brutal": 2,
+   "passion": 0,
+   "beginning": false,
+   "middle": false,
+   "end": true
+ },
+ {
+   "ID": 2,
+   "src": "panels/bestBros.png",
+   "brutal": 2,
+   "passion": 4,
+   "beginning": true,
+   "middle": true,
+   "end": true
+ },
+ {
+   "ID": 3,
+   "src": "panels/nightingale.png",
+   "brutal": 10,
+   "passion": 5,
+   "beginning": false,
+   "middle": true,
+   "end": true
+ },
+ {
+   "ID": 4,
+   "src": "panels/fuckYourWife.png",
+   "brutal": 5,
+   "passion": 5,
+   "beginning": true,
+   "middle": false,
+   "end": true
+ },
+ {
+   "ID": 5,
+   "src": "panels/pregoLady.png",
+   "brutal": 0,
+   "passion": 5,
+   "beginning": false,
+   "middle": true,
+   "end": true
+ },
+ {
+   "ID": 6,
+   "src": "panels/treeBaby.png",
+   "brutal": 2,
+   "passion": 0,
+   "beginning": true,
+   "middle": false,
+   "end": true
+ },
+ {
+   "ID": 7,
+   "src": "panels/fuckingTower.png",
+   "brutal": 6,
+   "passion": 3,
+   "beginning": true,
+   "middle": true,
+   "end": false
+ },
+ {
+   "ID": 8,
+   "src": "panels/neverBetrayYouLady.png",
+   "brutal": 0,
+   "passion": 10,
+   "beginning": true,
+   "middle": false,
+   "end": false
+ },
+ {
+   "ID": 9,
+   "src": "panels/becomeANun.png",
+   "brutal": 3,
+   "passion": 0,
+   "beginning": false,
+   "middle": true,
+   "end": false
+ },
+ {
+   "ID": 10,
+   "src": "panels/anOmenShrooms.png",
+   "brutal": 0,
+   "passion": 5,
+   "beginning": true,
+   "middle": true,
+   "end": false
+ },
+ {
+   "ID": 11,
+   "src": "panels/betterKisser.png",
+   "brutal": 2,
+   "passion": 10,
+   "beginning": true,
+   "middle": true,
+   "end": false
+ },
+ {
+   "ID": 12,
+   "src": "panels/myDudesAWar.png",
+   "brutal": 8,
+   "passion": 0,
+   "beginning": true,
+   "middle": true,
+   "end": true
+ },
+ {
+   "ID": 13,
+   "src": "panels/killYourHusband.png",
+   "brutal": 6,
+   "passion": 10,
+   "beginning": true,
+   "middle": true,
+   "end": false
+ },
+ {
+   "ID": 14,
+   "src": "panels/aFestival.png",
+   "brutal": 3,
+   "passion": 0,
+   "beginning": true,
+   "middle": true,
+   "end": false
+ },
+ {
+   "ID": 15,
+   "src": "panels/deadKnightSad.png",
+   "brutal": 6,
+   "passion": 7,
+   "beginning": false,
+   "middle": true,
+   "end": true
+ },
+ {
+   "ID": 16,
+   "src": "panels/swanHelp.png",
+   "brutal": 3,
+   "passion": 4,
+   "beginning": true,
+   "middle": true,
+   "end": false
+ },
+ {
+   "ID": 17,
+   "src": "panels/towerVisit.png",
+   "brutal": 3,
+   "passion": 8,
+   "beginning": true,
+   "middle": true,
+   "end": false
+ },
+ {
+   "ID": 18,
+   "src": "panels/listenNightingale.png",
+   "brutal": 1,
+   "passion": 6,
+   "beginning": true,
+   "middle": true,
+   "end": false
+ },
+ {
+   "ID": 19,
+   "src": "panels/goodKnightJoust.png",
+   "brutal": 4,
+   "passion": 0,
+   "beginning": false,
+   "middle": true,
+   "end": true
+ },
+ {
+   "ID": 20,
+   "src": "panels/badKnightJoust.png",
+   "brutal": 7,
+   "passion": 4,
+   "beginning": false,
+   "middle": true,
+   "end": true
+ },
+ {
+   "ID": 21,
+   "src": "panels/nunsRaiseBaby.png",
+   "brutal": 0,
+   "passion": 0,
+   "beginning": true,
+   "middle": false,
+   "end": true
+ },
+ {
+   "ID": 22,
+   "src": "panels/inspireLoyalty.png",
+   "brutal": 5,
+   "passion": 0,
+   "beginning": true,
+   "middle": true,
+   "end": false
+ }
 ]
-
-
-
-
---
+++ dataTest.js
@@ -0,0 +1,207 @@
+// Panel objects for Lais generation
+var laiPanelsTest = [
+	{
+		brutal: 5,
+		passion: 5,
+		src: "panels/fuckYourWife.png",
+		beginning: true,
+		middle: false,
+		end: false
+	},
+
+	{
+		brutal: 0,
+		passion: 0,
+		src: "panels/nuns1.png",
+		alt: "one nun saying 'I guess i'll become a nun'",
+		beginning: false,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 0,
+		passion: 0,
+		src: "panels/nunsSayNo.jpg",
+		beginning: false,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 0,
+		passion: 0,
+		src: "panels/treeBaby.jpg",
+		beginning: true,
+		middle: false,
+		end: true
+	},
+
+	{
+		brutal: 0,
+		passion: 0,
+		src: "panels/nuns2.png",
+		alt: "two happy nuns",
+		beginning: false,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 0,
+		passion: 2,
+		src: "panels/towers.png",
+		alt: "two towers facing each other, the lady says 'i'm so in love' and the dude says 'yah me too'",
+		beginning: true,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 4,
+		passion: 0,
+		src: "panels/werewolf.png",
+		beginning: false,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 0,
+		passion: 6,
+		src: "panels/neverBetray.png",
+		beginning: true,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 1,
+		passion: 2,
+		src: "panels/bestBros.png",
+		beginning: true,
+		middle: false,
+		end: false
+	},
+
+	{
+		brutal: 8,
+		passion: 0,
+		src: "panels/anOmen.jpg",
+		beginning: true,
+		middle: true,
+		end: false
+	},
+
+	{
+		brutal: 10,
+		passion: 0,
+		src: "panels/realDad.png",
+		beginning: false,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 2,
+		passion: 0,
+		src: "panels/hideClothes.png",
+		beginning: true,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 10,
+		passion: 0,
+		src: "panels/nightingale.png",
+		beginning: false,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 6,
+		passion: 0,
+		src: "panels/boat.png",
+		beginning: false,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 2,
+		passion: 0,
+		src: "panels/fuckingTower.png",
+		beginning: true,
+		middle: false,
+		end: true
+	},
+
+	{
+		brutal: 2,
+		passion: 10,
+		src: "panels/dieFuckMe.png",
+		beginning: false,
+		middle: true,
+		end: false
+	},
+
+	{
+		brutal: 0,
+		passion: 8,
+		src: "panels/allWomens.png",
+		beginning: false,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 0,
+		passion: 2,
+		src: "panels/broGrabs.png",
+		beginning: false,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 4,
+		passion: 0,
+		src: "panels/inspireLoyalty.png",
+		beginning: true,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 0,
+		passion: 6,
+		src: "panels/powerDynamic.png",
+		beginning: false,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 6,
+		passion: 6,
+		src: "panels/killHusband.png",
+		beginning: false,
+		middle: true,
+		end: true
+	},
+
+	{
+		brutal: 8,
+		passion: 0,
+		src: "panels/brobaths.png",
+		beginning: false,
+		middle: true,
+		end: true
+	}
+]
+
+
+
+
